ba9519640c04a594d394cf6d5c60608024a9bfe5
yet92 <zyzy92187@gmail.com>
2025-05-13 19:47:16 +0300

    Genre name unique now

--- theatre/models.py
+++ theatre/models.py
@@ -11,7 +11,7 @@ class Actor(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
